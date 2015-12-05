0c00cb226c4906dcc45b0633164751fc372306e3
treeherder <b.d.reddybest@gmail.com>
2015-12-05 05:39:01 +0000

    process superclass instead of procedural approach

--- src/stereocam.py
+++ src/stereocam.py
@@ -3,42 +3,48 @@ from multiprocessing import Process, Queue
 from PIL import Image
 import cv2
 import numpy as np
-import time
+from datetime import datetime
 
-def frame_feed(task, angle, outqueue):
-   while True:
-      frame = task.get()  #get frame from process queue
-      if frame is None:
-         break
-      (h, w) = frame.shape[:2]
-      center = (w / 2, h / 2)  #incase we change the shape/size farther down the line
-      M = cv2.getRotationMatrix2D(center, angle, 1.0)
-      frame = cv2.warpAffine(frame, M, (w, h))  #remap rotated frame to orignal
+class Camera(Process):
+   def __init__(self, task, angle):
+      super(Camera, self).__init__()
+      self.task = task
+      self.angle = angle
+
+   def frame_feed(self):
+      while True:
+         frame = self.task.get()  #get frame from process queue
+         if frame is None:
+            break
+         (h, w) = frame.shape[:2]
+         center = (w / 2, h / 2)  #incase we change the shape/size farther down the line
+         M = cv2.getRotationMatrix2D(center, seelf.angle, 1.0)
+         frame = cv2.warpAffine(frame, M, (w, h))  #remap rotated frame to orignal
   
 
 
 if __name__ == '__main__':
-   task_0 = Queue()
-   task_1 = Queue()
+   task_right = Queue()
+   task_left = Queue()
    con_task = Queue()
 
    capture_0 = cv2.VideoCapture(0)
    capture_1 = cv2.VideoCapture(1)
 
-   left_cam = Process(target=frame_feed, args=(task_1, -90, con_task))
+   left_cam = Camera(task_left, -90,)
    left_cam.start()
-   right_cam = Process(target=frame_feed, args=(task_0, 90, con_task))
+   right_cam = Camera(task_right, 90,)
    right_cam.start()
    while True:
       flag0, frame0=capture_0.read()
       flag1, frame1=capture_1.read()
 
-      task_0.put(frame0)
-      task_1.put(frame1)
+      task_right.put(frame0)
+      task_left.put(frame1)
 
       combo = np.concatenate((frame0, frame1), axis=1)
 
-      cv2.imshow('combined output  {0}'.format(time.Now()), combo)
+      cv2.imshow('combined output', combo)
       if (cv2.waitKey(1) & 0xFF) == ord('q'):
          print ("user abort by input")
          cv2.destroyAllWindows()
