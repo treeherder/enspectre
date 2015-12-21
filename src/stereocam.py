969522e9963e2dea0fc6d64c7317038cb7a19288
treeherder <b.d.reddybest@gmail.com>
2015-12-21 13:26:52 +0000

    selecting region of interest

--- src/stereocam.py
+++ src/stereocam.py
@@ -52,6 +52,7 @@ class Printr(Process):
       sys.stdout = open(str(os.getpid()) + ".out", "a")
       sys.stderr = open(str(os.getpid()) + "_error.out", "a")
 
+
 class Feature():
    def __init__(self, cap_idx):
       self.cap = cv2.VideoCapture(cap_idx)
@@ -64,6 +65,19 @@ class Feature():
       self.prvs = cv2.cvtColor(self.frame1,cv2.COLOR_BGR2GRAY)
       self.hsv = np.zeros_like(self.frame1)
       self.hsv[...,1] = 255
+
+      r,h,c,w = 200,20,300,20  #row, height, column, width  
+      self.track_window = (r,h,c,w)
+      self.roi = self.frame[r:r+h, c:c+w]
+      self.hsv_roi =  cv2.cvtColor(self.frame1, cv2.COLOR_BGR2HSV)
+      self.mask = cv2.inRange(self.hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+      self.roi_hist = cv2.calcHist([self.hsv_roi],[0],self.mask,[180],[0,180])
+      cv2.normalize(self.roi_hist,self.roi_hist,0,255,cv2.NORM_MINMAX)
+
+      # Setup the termination criteria, either 10 iteration or move by at least 1 pt
+      self.term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+
+
    def detect(self):
       self.ret, self.frame2 = self.cap.read()
       self.next = cv2.cvtColor(self.frame2,cv2.COLOR_BGR2GRAY)
@@ -72,8 +86,16 @@ class Feature():
       self.hsv[...,0] = self.ang*180/np.pi/2
       self.hsv[...,2] = cv2.normalize(self.mag,None,0,255,cv2.NORM_MINMAX)
       self.rgb = cv2.cvtColor(self.hsv,cv2.COLOR_HSV2BGR)
-      return (self.rgb)
 
+      self.dst = cv2.calcBackProject([self.rgb],[0],self.roi_hist,[0,180],1)
+      # apply meanshift to get the new location
+      self.ret, self.track_window = cv2.meanShift(self.dst, self.track_window, self.term_crit)
+
+      # Draw it on image
+      self.x,self.y,self.w,self.h = self.track_window
+      self.img2 = cv2.rectangle(self.rgb, (self.x,self.y), (self.x+self.w,self.y+self.h), 255,2)
+      return (self.img2)
+      
 
 
 if __name__ == '__main__':
@@ -99,12 +121,12 @@ if __name__ == '__main__':
    #enable Printer for debugging log
    while True:
 
-      #left_image=left_handle.detect()
-      left_image=left_handle.unfiltered()
+      left_image=left_handle.detect()
+      #left_image=left_handle.unfiltered()
 
-      right_image=right_handle.unfiltered()
+      #right_image=right_handle.unfiltered()
 
-      #right_image=right_handle.detect()
+      right_image=right_handle.detect()
 
       leftqueue.put(left_image)
       rightqueue.put(right_image)
